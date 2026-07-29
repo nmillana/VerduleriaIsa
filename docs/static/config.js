b3e806172e3ed61b99efbadd976416c69a3c2ab4
Natalia <nmillana@eess.cl>
2026-07-29 13:24:49 -0400

    Point Pages app to active Supabase project

--- docs/static/config.js
+++ docs/static/config.js
@@ -1,4 +1,4 @@
 window.VERDULERIA_CONFIG = {
-    SUPABASE_URL: "https://wbpknauplyxshqpqzjvv.supabase.co",
-    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndicGtuYXVwbHl4c2hxcHF6anZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyODM3MDAsImV4cCI6MjA5MTg1OTcwMH0.i2z6TBYtoixyj2ZdLGXmQ_-ZdTJ5vJi8Xv3mEF4qR2o",
+    SUPABASE_URL: "https://keqqojqcnqporvgsaqby.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtlcXFvanFjbnFwb3J2Z3NhcWJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1MjA2ODQsImV4cCI6MjA3ODA5NjY4NH0._m1jqZfsUyrJ_x4_FC_LVBLRYSDl2hp22obCrW5jFaY",
 };
