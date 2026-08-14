5832c95451a20242bda80b6d2939313ffdca1ad6
Natalia <nmillana@eess.cl>
2026-08-14 10:38:46 -0400

    Compact desktop home layout

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "verduleria-isa-pwa-v20260814-product-image-upload";
+const CACHE_NAME = "verduleria-isa-pwa-v20260814-desktop-home-compact";
 
 self.addEventListener("install", (event) => {
     self.skipWaiting();
